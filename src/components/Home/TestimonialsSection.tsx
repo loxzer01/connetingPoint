587e6e17798ee74072eb8dc9f76ad62c7fcd8588
Loxzer01 <javierover04@gmail.com>
2025-02-18 22:39:27 +0000

    correccion de build

--- src/components/Home/TestimonialsSection.tsx
+++ src/components/Home/TestimonialsSection.tsx
@@ -16,6 +16,7 @@ export function TestimonialsCTASection() {
 
   // Se asume que t("testimonialsCTASection.testimonials") retorna un array de strings.
   const testimonials = t("testimonialsCTASection.testimonials") as unknown as string[];
+  // console.log(testimonials);
 
   return (
     <section className="relative z-10 pt-[120px] bg-[rgba(11,17,58,1)]">
